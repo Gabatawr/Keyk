e93783fd72bd87f0bbdf49731603f4c56c622c1e
Gabatawr <gabatawr@gmail.com>
2020-12-13 11:16:54 +0200

    save

---
+++ Keyk/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using Keyk.Infrastructure.Commands.Base;
+
+namespace Keyk.Infrastructure.Commands
+{
+    class MinimizeApplicationCommand : Command
+    {
+        public override void Execute(object parameter) => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        public override bool CanExecute(object parameter) => true;
+    }
+}
---
+++ Keyk/Infrastructure/Commands/MinimizeApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using Keyk.Infrastructure.Commands.Base;
+
+namespace Keyk.Infrastructure.Commands
+{
+    class CloseApplicationCommand : Command
+    {
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+        public override bool CanExecute(object parameter) => true;
+    }
+}
