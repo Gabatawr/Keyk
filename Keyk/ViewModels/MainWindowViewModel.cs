b5c4da0bdcd483ce264a48108118153ab6770e0f
Gabatawr <gabatawr@gmail.com>
2020-12-13 23:55:21 +0200

    fix

--- Keyk/ViewModels/MainWindowViewModel.cs
+++ Keyk/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using Keyk.Infrastructure.Commands;
