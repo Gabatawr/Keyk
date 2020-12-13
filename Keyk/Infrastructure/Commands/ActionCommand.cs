3c62ed801262d9296ec0847eb95cad5df97f0a16
Gabatawr <gabatawr@gmail.com>
2020-12-13 23:28:40 +0200

    boom

--- Keyk/Infrastructure/Commands/ActionCommand.cs
+++ Keyk/Infrastructure/Commands/ActionCommand.cs
@@ -8,13 +8,13 @@ namespace Keyk.Infrastructure.Commands
         private readonly Action<object> _Execute;
         private readonly Func<object, bool> _CanExecute;
 
-        public ActionCommand(Action<object> Execute, Func<object, bool> CanExecute)
+        public ActionCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
         {
             _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
             _CanExecute = CanExecute;
         }
 
         public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
-        public override void Execute(object parameter) => _Execute(parameter);
+        public override void Execute(object parameter) { if (_CanExecute(parameter)) _Execute(parameter); }
     }
 }
--- Keyk/ViewModels/MainWindowViewModel.cs
+++ Keyk/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,69 @@
-﻿using Keyk.ViewModels.Base;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using Keyk.Infrastructure.Commands;
+using Keyk.Infrastructure.Commands.Base;
+using Keyk.ViewModels.Base;
 
 namespace Keyk.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Commands
 
+        #region Command : _MoveApplicationCommand
+
+        private Command _MoveApplicationCommand;
+        public Command MoveApplicationCommand
+        {
+            get => _MoveApplicationCommand ?? new ActionCommand
+                (
+                    param => ExecuteMoveApplicationCommand((MouseEventArgs)param),
+                    param => CanExecuteMoveApplicationCommand((MouseEventArgs)param)
+                );
+            set => _MoveApplicationCommand = value;
+        }
+
+        private void ExecuteMoveApplicationCommand(MouseEventArgs e) => Application.Current.MainWindow?.DragMove();
+        private bool CanExecuteMoveApplicationCommand(MouseEventArgs e) => ((FrameworkElement)e.Source) == Application.Current.MainWindow;
+
+        #endregion
+
+        #region Command : _DifficultyChangedCommand
+
+        private Command _DifficultyChangedCommand;
+        public Command DifficultyChangedCommand
+        {
+            get => _DifficultyChangedCommand ?? new ActionCommand
+                (
+                    param => ExecuteDifficultyChangedCommand((MouseEventArgs)param),
+                    param => CanExecuteDifficultyChangedCommand((MouseEventArgs)param)
+                );
+            set => _DifficultyChangedCommand = value;
+        }
+
+        private void ExecuteDifficultyChangedCommand(MouseEventArgs e) => Difficulty = e.GetPosition((IInputElement)e.Source).X / ((FrameworkElement)e.Source).ActualWidth;
+        private bool CanExecuteDifficultyChangedCommand(MouseEventArgs e) => e.LeftButton == MouseButtonState.Pressed;
+
+        #endregion
+
+        #endregion
+
+        #region Properties
+
+        #region double : _Difficulty
+
+        private double _Difficulty = 0.2;
+
+        /// <summary>Difficulty level</summary>
+        public double Difficulty
+        {
+            get => _Difficulty;
+            set => Set(ref _Difficulty, value);
+        }
+
+        #endregion
+
+        #endregion
     }
 }
