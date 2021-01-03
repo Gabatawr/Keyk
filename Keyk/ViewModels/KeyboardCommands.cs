8d805e4467db8adb80069d599a513f663b20132e
Gabatawr <gabatawr@gmail.com>
2021-01-03 19:32:20 +0200

    save

---
+++ Keyk/ViewModels/KeyboardCommands.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Keyk.ViewModels
+{
+    public class KeyboardCommands
+    {
+        #region ShiftUp
+
+        public static readonly DependencyProperty ShiftUpProperty = DependencyProperty.RegisterAttached
+        (
+            "ShiftUp", typeof(ICommand), typeof(KeyboardCommands),
+            new FrameworkPropertyMetadata(new PropertyChangedCallback(ShiftUpChanged))
+        );
+        private static void element_ShiftUp(object sender, KeyEventArgs e) => GetShiftUp((FrameworkElement)sender).Execute(e);
+        private static void ShiftUpChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((FrameworkElement)d).KeyUp += element_ShiftUp;
+        public static ICommand GetShiftUp(UIElement element) => (ICommand)element.GetValue(ShiftUpProperty);
+        public static void SetShiftUp(UIElement element, ICommand value) => element.SetValue(ShiftUpProperty, value);
+        #endregion
+
+        #region ShiftDown
+
+        public static readonly DependencyProperty ShiftDownProperty = DependencyProperty.RegisterAttached
+        (
+            "ShiftDown", typeof(ICommand), typeof(KeyboardCommands),
+            new FrameworkPropertyMetadata(new PropertyChangedCallback(ShiftDownChanged))
+        );
+        private static void element_ShiftDown(object sender, KeyEventArgs e) => GetShiftDown((FrameworkElement)sender).Execute(e);
+        private static void ShiftDownChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((FrameworkElement)d).KeyDown += element_ShiftDown;
+        public static ICommand GetShiftDown(UIElement element) => (ICommand)element.GetValue(ShiftDownProperty);
+        public static void SetShiftDown(UIElement element, ICommand value) => element.SetValue(ShiftDownProperty, value);
+
+        #endregion
+
+        #region CapsLockDown
+
+        public static readonly DependencyProperty CapsLockDownProperty = DependencyProperty.RegisterAttached
+        (
+            "CapsLockDown", typeof(ICommand), typeof(KeyboardCommands),
+            new FrameworkPropertyMetadata(new PropertyChangedCallback(CapsLockDownChanged))
+        );
+        private static void element_CapsLockDown(object sender, KeyEventArgs e) => GetCapsLockDown((FrameworkElement)sender).Execute(e);
+        private static void CapsLockDownChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((FrameworkElement)d).KeyDown += element_CapsLockDown;
+        public static ICommand GetCapsLockDown(UIElement element) => (ICommand)element.GetValue(CapsLockDownProperty);
+        public static void SetCapsLockDown(UIElement element, ICommand value) => element.SetValue(CapsLockDownProperty, value);
+
+        #endregion
+    }
+}
--- Keyk/ViewModels/MainWindowViewModel.cs
+++ Keyk/ViewModels/MainWindowViewModel.cs
@@ -8,6 +8,12 @@ namespace Keyk.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        public MainWindowViewModel()
+        {
+            Symbols = (Keyboard.IsKeyToggled(Key.CapsLock)) ? _symbolsShift : _symbols;
+            CommandKey = _commandKey;
+        }
+
         #region Commands
         //------------------------------------------------------------------------------
 
@@ -49,6 +55,75 @@ namespace Keyk.ViewModels
         //--------------------------------------------------------------------
         #endregion
 
+        #region Command : _ShiftDownCommand
+        //--------------------------------------------------------------------
+
+        private Command _ShiftDownCommand;
+        public Command ShiftDownCommand
+        {
+            get => _ShiftDownCommand ?? new ActionCommand
+            (
+                param => ExecuteShiftDownCommand((KeyEventArgs)param),
+                param => CanExecuteShiftDownCommand((KeyEventArgs)param)
+            );
+            set => _ShiftDownCommand = value;
+        }
+
+        private void ExecuteShiftDownCommand(KeyEventArgs e)
+        {
+            Symbols = (Keyboard.IsKeyToggled(Key.CapsLock)) ? _symbols : _symbolsShift;
+        }
+        private bool CanExecuteShiftDownCommand(KeyEventArgs e) => e.Key == Key.LeftShift || e.Key == Key.RightShift;
+
+        //--------------------------------------------------------------------
+        #endregion
+
+        #region Command : _ShiftUpCommand
+        //--------------------------------------------------------------------
+
+        private Command _ShiftUpCommand;
+        public Command ShiftUpCommand
+        {
+            get => _ShiftUpCommand ?? new ActionCommand
+            (
+                param => ExecuteShiftUpCommand((KeyEventArgs)param),
+                param => CanExecuteShiftUpCommand((KeyEventArgs)param)
+            );
+            set => _ShiftUpCommand = value;
+        }
+
+        private void ExecuteShiftUpCommand(KeyEventArgs e)
+        {
+            Symbols = (Keyboard.IsKeyToggled(Key.CapsLock)) ? _symbolsShift : _symbols;
+        }
+        private bool CanExecuteShiftUpCommand(KeyEventArgs e) => e.Key == Key.LeftShift || e.Key == Key.RightShift;
+
+        //--------------------------------------------------------------------
+        #endregion
+
+        #region Command : _CapsLockDownCommand
+        //--------------------------------------------------------------------
+
+        private Command _CapsLockDownCommand;
+        public Command CapsLockDownCommand
+        {
+            get => _CapsLockDownCommand ?? new ActionCommand
+            (
+                param => ExecuteCapsLockDownCommand((KeyEventArgs)param),
+                param => CanExecuteCapsLockDownCommand((KeyEventArgs)param)
+            );
+            set => _CapsLockDownCommand = value;
+        }
+
+        private void ExecuteCapsLockDownCommand(KeyEventArgs e)
+        {
+            Symbols = (Keyboard.IsKeyToggled(Key.CapsLock)) ? _symbolsShift : _symbols;
+        }
+        private bool CanExecuteCapsLockDownCommand(KeyEventArgs e) => e.Key == Key.CapsLock;
+
+        //--------------------------------------------------------------------
+        #endregion
+
         //------------------------------------------------------------------------------
         #endregion
 
@@ -64,9 +139,63 @@ namespace Keyk.ViewModels
             set => Set(ref _Difficulty, value);
         }
         //---------------------------------------------------------------------
-        #endregion 
+        #endregion
+
+        #region Symbols
+
+        private readonly char[] _symbols = new[]
+        {
+            '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+            'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+            'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'',
+            'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+        };
+        private readonly char[] _symbolsShift = new[]
+        {
+            '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
+            'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|',
+            'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"',
+            'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?',
+        };
+
+        #region char[] : _Symbols
+        //---------------------------------------------------------------------
+
+        private char[] _Symbols;
+        public char[] Symbols
+        {
+            get => _Symbols;
+            set => Set(ref _Symbols, value);
+        }
+
+        //---------------------------------------------------------------------
+        #endregion
+
+        private readonly string[] _commandKey = new[]
+        {
+            "Backspace",
+            "Tab",
+            "Caps Lock", "Enter",
+            "Shift",
+            "Ctrl", "Win", "Alt", "Space"
+        };
+
+        #region string[] : _CommandKey
+        //---------------------------------------------------------------------
+
+        private string[] _CommandKey;
+        public string[] CommandKey
+        {
+            get => _CommandKey;
+            set => Set(ref _CommandKey, value);
+        }
+
+        //---------------------------------------------------------------------
+        #endregion
+
+        #endregion
 
         //------------------------------------------------------------------------------
-        #endregion 
+        #endregion
     }
 }
